f4689303d5e670e7ed0d6f83b57bbc0a5cda1b6e
Joseph <joerodrig3@gmail.com>
2016-11-04 23:25:46 -0400

    Validate base path is real, scaffolding containers parser

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 #!/usr/bin/env node
 const program = require('commander');
 const fs = require('fs');
-
+const textFilesLoader = require('text-files-loader');
+const {validateDirectory, die} = require('./helpers.js');
 
 class Parser {
   constructor() {
     this.path = this._initializePath();
+    this.containerDeps = this._containerDependencies();
   }
 
+  // Validate and return the path passed into the script
   _initializePath() {
+    let argPath = "";
     program
-      .arguments('<path/to/components')
-      .action( (path) => {
-        console.log(`path: ${path}`)
-      })
-      .parse(process.argv);
+     .arguments('<path/to/components')
+     .action((basePath) => {
+       const validatedDirectory = validateDirectory(basePath);
+       if (validatedDirectory.isValid ) {
+        argPath = basePath;
+       } else {
+        die(validatedDirectory.error);
+       }
+     })
+     .parse(process.argv);
+
+    return argPath;
+  }
+
+  _containerDependencies() {
+    textFilesLoader.load(`${this.path}/containers`, (err, files) => {
+      if (err) { die({ msg: err }) };
+
+      // console.log(files);
+    });
   }
 }
+
+
 
 const ReactParser = new Parser();
